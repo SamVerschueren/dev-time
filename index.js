986b0ca24b4736b1801ac1af2cc668a9c39f6f62
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-08 12:31:26 +0100

    implement fetch next push mechanism - fixes #1

--- index.js
+++ index.js
@@ -8,18 +8,47 @@ var del = require('del');
 var moment = require('moment-timezone');
 var objectAssign = require('object-assign');
 
+var dir;
+
 function clone(repo, dest) {
 	return pify(childProcess.execFile.bind(childProcess), Promise)('git', ['clone', 'https://github.com/' + repo + '.git', dest], {stdio: 'ignore'});
 }
 
 function extractOffset(push, dir) {
-	return clone(push.repo.name, dir).then(function () {
-		return pify(childProcess.execFile.bind(childProcess), Promise)('git', ['log', '-1', '--format="%aI"', push.payload.commits[0].sha], {encoding: 'utf8', cwd: dir});
-	});
+	return clone(push.repo.name, dir)
+		.then(function () {
+			return pify(childProcess.execFile.bind(childProcess), Promise)('git', ['log', '-1', '--format="%aI"', '--ignore-missing', push.payload.commits[0].sha], {encoding: 'utf8', cwd: dir});
+		})
+		.then(function (offset) {
+			return offset.trim();
+		});
 }
 
 function clean(dir) {
 	del.sync(dir, {force: true});
+}
+
+function fetchPush(user, opts) {
+	var pushId;
+
+	dir = tempfile();
+
+	return latestPush(user, opts)
+		.then(function (push) {
+			pushId = push.id;
+
+			return extractOffset(push, dir);
+		})
+		.then(function (offset) {
+			if (offset === '') {
+				clean(dir);
+
+				opts.exclude.push(pushId);
+				return fetchPush(user, opts);
+			}
+
+			return offset;
+		});
 }
 
 module.exports = function (user, opts) {
@@ -27,21 +56,16 @@ module.exports = function (user, opts) {
 		return Promise.reject(new TypeError('Expected a user'));
 	}
 
-	opts = objectAssign({}, opts, {pages: 10});
+	opts = objectAssign({exclude: []}, opts, {pages: 10});
 
-	var dir = tempfile();
-
-	return latestPush(user, opts)
-		.then(function (push) {
-			return extractOffset(push, dir);
-		})
+	return fetchPush(user, opts)
 		.then(function (offset) {
-			clean(dir);
+			clean();
 
 			return moment.utc().utcOffset(offset).format();
 		})
 		.catch(function (err) {
-			clean(dir);
+			clean();
 
 			throw err;
 		});
